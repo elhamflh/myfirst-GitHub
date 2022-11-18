b726f9fc233bcb7d031984f21fbd40a9b05c2314
eskandar <eskandari.m1991@gmail.com>
2022-11-18 04:22:58 +0330

    creat root page+ posts+ singllepost -image not loading

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
-from .models import Article,Authur
-
+from . import models
 # Register your models here.
 
-class ArticleAdmin(admin.ModelAdmin):
-    list_display= ("title","slug","publish","status")
-    list_filter = ("publish","status")
-    search_fields=("title","des")
-    prepopulated_fields={"slug": ("title",)}
-    ordering = ["-status","publish"]
-
-admin.site.register(Article,ArticleAdmin)
-admin.site.register(Authur)
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display= ("title","slug","publish","status")
+#     list_filter = ("publish","status")
+#     search_fields=("title","des")
+#     prepopulated_fields={"slug": ("title",)}
+#     ordering = ["-status","publish"]
+#  ArticleAdmin
+admin.site.register(models.Article)
+admin.site.register(models.Authur)
 
 
 
